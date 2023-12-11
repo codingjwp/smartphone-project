f94ad59560814a99ca5aab0a48f43cf06a6c32a6
codingjwp <codingjwp@gmail.com>
2023-12-11 11:42:55 +0900

    feat: store 변경으로 인한 부분 수정

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -4,13 +4,13 @@ import { usePhoneStore } from "../states/stores";
 import SearchBar from "./SearchBar";
 
 const SearchHeader = () => {
-  const categorys = usePhoneStore((state) => state.category);
+  const categorys = usePhoneStore((state) => state.categorys);
 
   return (
     <FilterGroup>
       <Selectors name="brand" title="브랜드" optionData={categorys} />
       <Selectors name="os" title="운영체제" optionData={["android", "ios", "coloros", "harmony", "wateros"]} />
-      <Selectors name="storage" title="용량" optionData={['8','16','32','64','128','256','512','1000']} />
+      <Selectors name="storage" title="용량" optionData={['8', '16', '32', '64', '128', '256', '512', '1000']} />
       <SearchBar />
     </FilterGroup>
   )
--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
 
 
-export const Toast = () => {
+const Toast = () => {
   const timeRef = useRef<number>()
   const [isOpen, setIsOpen] = useState(false);
   const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent | null>(null)
 
   const handelCloseToInstall = () => {
     setIsOpen(false);
-    if (!deferredPrompt) return ;
+    if (!deferredPrompt) return;
     deferredPrompt.prompt();
     setDeferredPrompt(null);
   }
@@ -18,9 +18,9 @@ export const Toast = () => {
       e.preventDefault();
       setDeferredPrompt(e as BeforeInstallPromptEvent);
       setIsOpen(true);
-      timeRef.current = setTimeout(()=> { setIsOpen(false) }, 5000)
+      timeRef.current = setTimeout(() => { setIsOpen(false) }, 5000)
     }
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);  
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
     return () => {
       window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
       if (timeRef.current) clearTimeout(timeRef.current);
@@ -30,7 +30,7 @@ export const Toast = () => {
   return (
     <div className={`fixed w-56 h-16 ${isOpen ? "top-10" : "-top-20"}  left-1/2 -translate-x-1/2 bg-cyan-900 rounded-md z-20`}>
       <div className='flex flex-row h-full items-center justify-evenly'>
-        <img className='rounded-2xl' src='/icons/iconX48.png' alt='smartphone data'/>
+        <img className='rounded-2xl' src='/icons/iconX48.png' alt='smartphone data' />
         <div className='flex flex-col justify-center gap-1 text-sm'>
           <p className='text-white'>스마트 정보</p>
           <button type='button' className='bg-transparent text-cyan-300' onClick={handelCloseToInstall}>설치</button>
@@ -39,3 +39,5 @@ export const Toast = () => {
     </div>
   )
 }
+
+export default Toast;
