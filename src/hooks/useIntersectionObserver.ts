d9a16d49d0146c1f7b4b93e927e36fb5242bb2b9
codingjwp <codingjwp@gmail.com>
2023-12-13 16:16:04 +0900

    feat: intersectionObserver 수정

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -6,44 +6,13 @@ export const useIntersectionObserver = <T extends HTMLElement>(
 ) => {
   const observer = useRef<IntersectionObserver | null>(null);
   const ref = useCallback((node: T) => {
-    if(deps.every(Boolean)) {
+    if (deps.every(Boolean)) {
       observer.current?.disconnect();
       observer.current = new IntersectionObserver((entries) => {
-        if(entries[0].isIntersecting) callback();
+        if (entries[0].isIntersecting) callback();
       })
       if (node) observer.current.observe(node)
     }
   }, [deps, callback])
   return ref;
 }
-
-// export const useScrollPage = () => {
-//   const {pages, setNextPage} = usePhoneStore((state)=> ({pages: state.pages, setNextPage: state.setNextPage}))
-//   const ref = useRef<HTMLDivElement>(null);
-//   useEffect(() => {
-//     if (!ref || !ref.current) return;
-    
-//     const isObserver = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
-//       entries.forEach((entry) => {
-//         if (pages.page !== pages.totalPage && entry.isIntersecting && entry.intersectionRatio > 0) {
-//           observer.unobserve(entry.target);
-//           setNextPage();
-//         }
-//       })
-//     }
-
-//     const options = {
-//       root: null,
-//       rootMargin: '0px 0px 0px 0px',
-//       threshold: [0, 1]
-//     }
-
-//     const observer = new IntersectionObserver(isObserver, options);
-//     observer.observe(ref.current);
-
-//     return () => {
-//       observer.disconnect();
-//     }
-//   }, [pages.page, pages.totalPage, setNextPage])
-//   return {ref};
-// }
